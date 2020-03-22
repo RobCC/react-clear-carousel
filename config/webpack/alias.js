265ab922067c9d0d6d9d87cc64d0227e8e7c7d8c
José Roberto Chávez Cobián <rrc0138@gmail.com>
2020-03-22 17:42:16 -0600

    Remove all

--- config/webpack/alias.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-const src = path.resolve(__dirname, '../../src');
-const root = path.resolve(__dirname, '../..');
-
-module.exports = {
-  '#': src,
-  '~': root,
-};
